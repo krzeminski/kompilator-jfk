ab6f4d685da347a4425be84f0e084b9a24c0e43a
krzeminski <adriankrzeminskki@gmail.com>
2023-06-02 01:38:30 +0200

    update grammar to contain read and write

---
+++ grammar/main.py
@@ -0,0 +1,19 @@
+from antlr4 import *
+from DallasLexer import DallasLexer
+from DallasParser import DallasParser
+from LLVMActions import LLVMActions
+
+
+def main():
+    input_stream = FileStream("test.Dallas")
+    lexer = DallasLexer(input_stream)
+    token_stream = CommonTokenStream(lexer)
+    parser = DallasParser(token_stream)
+
+    tree = parser.prog()
+
+    walker = ParseTreeWalker()
+    walker.walk(LLVMActions(), tree)
+
+if __name__ == '__main__':
+    main()
